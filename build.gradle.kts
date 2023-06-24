4e0786aa0fa79b3c8bd6c40ee6458a98d9b5f060
HOJUN <bmp.tom@yahoo.com>
2023-06-24 18:54:20 +0900

    [add] initial project

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("org.springframework.boot") version "3.1.1"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // implementation("org.postgresql:postgresql")
+    implementation("org.jetbrains.exposed:exposed-core:0.35.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.35.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.35.1")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/example/demo/MyHelloWorldApplication.kt
@@ -0,0 +1,13 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun helloWorld(): String {
+        return "Hello, World!"
+    }
+}
