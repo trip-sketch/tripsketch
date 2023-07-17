f1b79bcf61686f6ca5e34671c881e6c72d5f876c
HOJUN <bmp.tom@yahoo.com>
2023-07-17 17:26:28 +0900

    [modify] body to header

--- src/main/kotlin/kr/kro/tripsketch/repositories/UserRepository.kt
+++ src/main/kotlin/kr/kro/tripsketch/repositories/UserRepository.kt
@@ -2,7 +2,9 @@ package kr.kro.tripsketch.repositories
 
 import kr.kro.tripsketch.domain.User
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository : MongoRepository<User, String> {
 
     fun findByEmail(email: String): User?
