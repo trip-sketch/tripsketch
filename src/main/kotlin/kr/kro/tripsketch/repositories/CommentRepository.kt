69ce8970c40e929d0a4a780fee22e80e1bed195d
HOJUN <bmp.tom@yahoo.com>
2023-07-18 17:48:43 +0900

    [lint] linting

--- src/main/kotlin/kr/kro/tripsketch/repositories/CommentRepository.kt
+++ src/main/kotlin/kr/kro/tripsketch/repositories/CommentRepository.kt
@@ -5,5 +5,4 @@ import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CommentRepository : MongoRepository<Comment, String> {
-}
+interface CommentRepository : MongoRepository<Comment, String>
