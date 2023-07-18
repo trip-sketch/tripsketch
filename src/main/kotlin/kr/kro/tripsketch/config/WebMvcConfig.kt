144c0af12a227bb36bff9ca2696037de146c183b
HOJUN <bmp.tom@yahoo.com>
2023-07-18 16:59:57 +0900

    [modify] WebMvcConfig

--- src/main/kotlin/kr/kro/tripsketch/config/WebMvcConfig.kt
+++ src/main/kotlin/kr/kro/tripsketch/config/WebMvcConfig.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+
 @Configuration
 class WebMvcConfig(private val jwtService: JwtService) : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
