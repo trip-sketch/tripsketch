b56cbf7bf50b74adf3ebb94d873541a7d5aab3ae
cobaltcyan <cobaltcyan.park@gmail.com>
2023-07-25 16:34:55 +0900

    [Fix] trip API 기본 CRUD 구현 진행 중

--- src/main/kotlin/kr/kro/tripsketch/domain/Trip.kt
+++ src/main/kotlin/kr/kro/tripsketch/domain/Trip.kt
@@ -5,41 +5,26 @@ import org.springframework.data.mongodb.core.mapping.Document
 import org.bson.types.ObjectId  // ObjectId import
 import java.time.LocalDateTime
 
-
 @Document(collection = "trips")
 data class Trip(
     // @Id var id: String? = null,
     @Id var id: ObjectId = ObjectId.get(),
+    // var userId: ObjectId,            // 자동생성되니까.. 일단 String으로 구현!
+    // var scheduleId: ObjectId,        // 자동생성되니까.. 일단 String으로 구현!
+    var userId: String,   
+    var scheduleId: String,
     var title: String,
     var content: String,
     var likes: Int,
     var views: Int,
+    var location: String? = null,
+    var startedAt: LocalDateTime = LocalDateTime.now(),
+    var endAt: LocalDateTime = LocalDateTime.now(),
+    var hashtag: String,
+    var hidden: Boolean = false,
     val createdAt: LocalDateTime = LocalDateTime.now(),
     var updatedAt: LocalDateTime? = null,
     var deletedAt: LocalDateTime? = null,
-    var hidden: Boolean = false
+    var likeFlag: Int = 0,
+    var tripViews: Set<String> = setOf()
 )
-
-// @Document(collection = "trips")
-// data class Trip(
-//     @Id val id: ObjectId = ObjectId.get(),     // ObjectId로 타입 변경
-//     val userId: ObjectId,         // ObjectId로 타입 변경
-//     val scheduleId: ObjectId,     // ObjectId로 타입 변경
-//     // @Id val id: String? = null,
-//     // val userId: String,                     // 외래키-User객체자체를 참조  // val userId: String,
-//     // val scheduleId: String,
-//     var title: String,
-//     var content: String,
-//     var likes: Int,
-//     var views: Int,
-//     var location: String? = null,
-//     var startedAt: LocalDateTime = LocalDateTime.now(),
-//     var endAt: LocalDateTime = LocalDateTime.now(),
-//     var hashtag: String,
-//     var hidden: Int,
-//     val createdAt: LocalDateTime = LocalDateTime.now(),
-//     var updatedAt: LocalDateTime? = null,
-//     var deletedAt: LocalDateTime? = null,
-//     var likeFlag: Int = 0,
-//     val tripViews: Set<String> = setOf(), // tripviews 배열로 set 형태로 받겠다?   - 원하면 redis 로도 해줘도 됌!
-// )
