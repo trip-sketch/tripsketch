c212815db2ef6c91845b9d5205027a8d2fc9b893
cobaltcyan <cobaltcyan.park@gmail.com>
2023-08-03 17:46:49 +0900

    [Feat] file 다운로드/업로드 구현 시작

---
+++ src/main/kotlin/kr/kro/tripsketch/repositories/FileRepository.kt
@@ -0,0 +1,12 @@
+package kr.kro.tripsketch.repositories
+
+import kr.kro.tripsketch.domain.Trip
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface FileRepository : MongoRepository<File, String> {
+    // 별도의 쿼리 메서드가 필요하면 여기에 추가 가능
+}
+
+
