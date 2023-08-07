f25018253c30920059afb6c32c05ac0ad0093976
HOJUN <bmp.tom@yahoo.com>
2023-08-07 12:42:14 +0900

    [modify] Follow Function

--- src/main/kotlin/kr/kro/tripsketch/services/FollowService.kt
+++ src/main/kotlin/kr/kro/tripsketch/services/FollowService.kt
@@ -2,13 +2,16 @@ package kr.kro.tripsketch.services
 
 import kr.kro.tripsketch.repositories.FollowRepository
 import kr.kro.tripsketch.domain.Follow
+import kr.kro.tripsketch.dto.UserProfileDto
+import kr.kro.tripsketch.repositories.UserRepository
 import org.springframework.stereotype.Service
 
 @Service
 class FollowService(
     private val followRepository: FollowRepository,
     private val userService: UserService,
-    private val jwtService: JwtService
+    private val jwtService: JwtService,
+    private val userRepository: UserRepository
 ) {
     fun follow(token: String, following: String) {
         val follower = jwtService.getEmailFromToken(token)
@@ -30,11 +33,25 @@ class FollowService(
         followRepository.deleteByFollowerAndFollowing(follower, following)
     }
 
-    fun getFollowings(follower: String): List<Follow> {
-        return followRepository.findByFollower(follower)
+    fun getFollowings(follower: String): List<UserProfileDto> {
+        return followRepository.findByFollower(follower).map { follow ->
+            val user = userRepository.findByNickname(follow.following)
+            UserProfileDto(
+                nickname = user?.nickname,
+                introduction = user?.introduction,
+                profileImageUrl = user?.profileImageUrl
+            )
+        }
     }
 
-    fun getFollowers(following: String): List<Follow> {
-        return followRepository.findByFollowing(following)
+    fun getFollowers(following: String): List<UserProfileDto> {
+        return followRepository.findByFollowing(following).map { follow ->
+            val user = userRepository.findByEmail(follow.follower)
+            UserProfileDto(
+                nickname = user?.nickname,
+                introduction = user?.introduction,
+                profileImageUrl = user?.profileImageUrl
+            )
+        }
     }
 }
