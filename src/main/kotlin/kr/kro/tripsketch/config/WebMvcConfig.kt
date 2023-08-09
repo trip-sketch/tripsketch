317637c8657eec093e3d55e1fc1bca265e03b1b1
고병욱 <rhquddnr95@naver.com>
2023-08-09 16:22:02 +0900

    [RollBack] config 롤

--- src/main/kotlin/kr/kro/tripsketch/config/WebMvcConfig.kt
+++ src/main/kotlin/kr/kro/tripsketch/config/WebMvcConfig.kt
@@ -39,6 +39,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebMvcConfig(private val jwtService: JwtService) : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(JwtTokenInterceptor(jwtService))
-            .addPathPatterns("hello")
+            .addPathPatterns("/**")
     }
 }
