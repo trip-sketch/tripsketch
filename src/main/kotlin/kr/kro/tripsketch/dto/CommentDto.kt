1ff2fbbb30471315180e74e69dccefee2c47d0d1
고병욱 <rhquddnr95@naver.com>
2023-08-11 14:50:46 +0900

    [modify] User 도메인 CommentDto에 주입

--- src/main/kotlin/kr/kro/tripsketch/dto/CommentDto.kt
+++ src/main/kotlin/kr/kro/tripsketch/dto/CommentDto.kt
@@ -2,12 +2,14 @@ package kr.kro.tripsketch.dto
 
 import kr.kro.tripsketch.domain.Comment
 import java.time.LocalDateTime
+import kr.kro.tripsketch.dto.UserProfileDto
+import kr.kro.tripsketch.repositories.UserRepository
 
 data class CommentDto(
     val id: String? = null,
     val userEmail: String,
     val userNickName: String,
-    val userProfileUrl: String, 
+    val userProfileUrl: String,
     val tripId: String,
     val parentId: String? = null,
     val content: String? = null,
@@ -17,14 +19,25 @@ data class CommentDto(
     val replyTo: String? = null,
     val isDeleted: Boolean = false,
     val children: MutableList<CommentDto> = mutableListOf(),
+    val userRepository: UserRepository, // userRepository 추가
 ) {
     companion object {
-        fun fromComment(comment: Comment): CommentDto {
+        fun fromComment(comment: Comment, userRepository: UserRepository): CommentDto {
+            val commenter = userRepository.findByEmail(comment.userEmail)
+            val commenterProfile = commenter?.let {
+                UserProfileDto(
+                    email = it.email,
+                    nickname = it.nickname,
+                    introduction = it.introduction,
+                    profileImageUrl = it.profileImageUrl
+                )
+            }
+
             return CommentDto(
                 id = comment.id,
                 userEmail = comment.userEmail,
-                userNickName = comment.userNickName,        
-                userProfileUrl = comment.userProfileUrl,  
+                userNickName = commenterProfile?.nickname ?: "", // 사용자가 없을 경우 대비
+                userProfileUrl = commenterProfile?.profileImageUrl ?: "", // 사용자가 없을 경우 대비
                 tripId = comment.tripId,
                 parentId = comment.parentId,
                 content = comment.content,
@@ -33,7 +46,8 @@ data class CommentDto(
                 likedBy = comment.likedBy.toMutableSet(),
                 replyTo = comment.replyTo,
                 isDeleted = comment.isDeleted,
-                children = comment.children.map { fromComment(it) }.toMutableList(), // MutableList로 변환
+                children = comment.children.map { fromComment(it, userRepository) }.toMutableList(),
+                userRepository = userRepository, // 의존성 주입
             )
         }
     }
