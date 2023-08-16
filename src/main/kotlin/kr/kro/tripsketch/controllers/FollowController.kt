2540e5bf2388727240d5a8aff56b6f15860552c5
HOJUN <bmp.tom@yahoo.com>
2023-08-16 17:56:52 +0900

    [add] TokenUtils

--- src/main/kotlin/kr/kro/tripsketch/controllers/FollowController.kt
+++ src/main/kotlin/kr/kro/tripsketch/controllers/FollowController.kt
@@ -6,6 +6,8 @@ import kr.kro.tripsketch.dto.FollowDto
 import kr.kro.tripsketch.dto.ProfileDto
 import kr.kro.tripsketch.dto.UserProfileDto
 import kr.kro.tripsketch.services.FollowService
+import kr.kro.tripsketch.services.JwtService
+import kr.kro.tripsketch.utils.TokenUtils
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
@@ -15,25 +17,25 @@ import org.springframework.web.bind.annotation.*
 @Validated
 @RestController
 @RequestMapping("api/follow")
-class FollowController(private val followService: FollowService) {
+class FollowController(private val followService: FollowService, private val jwtService: JwtService) {
 
     @PostMapping
     fun follow(@RequestHeader("Authorization") token: String, @Valid @RequestBody followDto: FollowDto): ResponseEntity<String> {
-        val actualToken = token.removePrefix("Bearer ").trim()
+        val actualToken = TokenUtils.validateAndExtractToken(jwtService, token)
         followService.follow(actualToken, followDto.nickname)
         return ResponseEntity.status(HttpStatus.OK).body("성공적으로 구독했습니다.")
     }
 
     @DeleteMapping
     fun unfollow(@RequestHeader("Authorization") token: String, @Valid @RequestBody followDto: FollowDto): ResponseEntity<String> {
-        val actualToken = token.removePrefix("Bearer ").trim()
+        val actualToken = TokenUtils.validateAndExtractToken(jwtService, token)
         followService.unfollow(actualToken, followDto.nickname)
         return ResponseEntity.status(HttpStatus.OK).body("구독 취소되었습니다. ")
     }
 
     @DeleteMapping("/unfollowMe")
     fun unfollowMe(@RequestHeader("Authorization") token: String, @Valid @RequestBody followDto: FollowDto): ResponseEntity<String> {
-        val actualToken = token.removePrefix("Bearer ").trim()
+        val actualToken = TokenUtils.validateAndExtractToken(jwtService, token)
         followService.unfollowMe(actualToken, followDto.nickname)
         return ResponseEntity.status(HttpStatus.OK).body("해당 사용자의 구독을 취소했습니다.")
     }
