9f0083ac212afced9e80348bd1a91246c1752fbb
HOJUN <bmp.tom@yahoo.com>
2023-08-21 15:36:01 +0900

    [fix] Token expired

--- src/main/kotlin/kr/kro/tripsketch/exceptions/CustomException.kt
+++ src/main/kotlin/kr/kro/tripsketch/exceptions/CustomException.kt
@@ -10,8 +10,10 @@ open class CustomException(
 enum class ErrorCode(val httpStatus: HttpStatus) {
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED),
     FORBIDDEN(HttpStatus.FORBIDDEN),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED),
     // 기타 다른 오류 코드...
 }
 
 class UnauthorizedException(message: String) : CustomException(ErrorCode.UNAUTHORIZED, message)
 class ForbiddenException(message: String) : CustomException(ErrorCode.FORBIDDEN, message)
+class CustomExpiredTokenException(message: String) : CustomException(ErrorCode.TOKEN_EXPIRED, message)
