5578df1a3b9a21e31bf30b87d27a7a7db412ac29
HOJUN <bmp.tom@yahoo.com>
2023-08-21 15:09:58 +0900

    [add] Validation Annotation

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     // 코틀린 리플렉션 기능
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+
     // 스프링 MVC를 사용한 웹 애플리케이션, RESTful 애플리케이션을 만들기 위한 스타터
     // 기본적으로 내장 컨테이너로 Tomcat을 사용합니다.
     implementation("org.springframework.boot:spring-boot-starter-web")
