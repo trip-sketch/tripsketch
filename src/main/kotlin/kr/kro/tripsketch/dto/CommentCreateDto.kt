64005a90cfd6109b2156c901adda263a3fd00089
고병욱 <rhquddnr95@naver.com>
2023-08-23 17:28:43 +0900

    [modify] CommentChildrenCreate 추가하여 댓글 대댓글 분리

--- src/main/kotlin/kr/kro/tripsketch/dto/CommentCreateDto.kt
+++ src/main/kotlin/kr/kro/tripsketch/dto/CommentCreateDto.kt
@@ -9,7 +9,4 @@ data class CommentCreateDto(
     @field:NotBlank(message = "댓글 내용(content)은 필수 항목입니다.")
     @field:Size(max = 200, message = "댓글 내용(content)은 200자 이하로 입력해주세요.")
     val content: String,
-
-    val parentId: String? = null,
-    val replyTo: String? = null
 )
