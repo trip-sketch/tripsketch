355f5bffa013ae020aff16d2a931de3e7951b96f
고병욱 <rhquddnr95@naver.com>
2023-08-23 17:26:22 +0900

    [add] CommentChildrenCreateDto dto 추가

---
+++ src/main/kotlin/kr/kro/tripsketch/dto/CommentChildrenCreateDto.kt
@@ -0,0 +1,15 @@
+package kr.kro.tripsketch.dto
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+data class CommentChildrenCreateDto(
+    @field:NotBlank(message = "게시물 아이디(tripId)는 필수 항목입니다.")
+    val tripId: String,
+
+    @field:NotBlank(message = "댓글 내용(content)은 필수 항목입니다.")
+    @field:Size(max = 200, message = "댓글 내용(content)은 200자 이하로 입력해주세요.")
+    val content: String,
+
+    @field:NotBlank(message = "답글 닉네임(replyTo)은  필수 항목입니다.")
+    val replyTo: String
+)
