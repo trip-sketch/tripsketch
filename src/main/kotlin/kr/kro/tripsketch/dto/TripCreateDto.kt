e0eaff94f2668d764158168e1ed10a59f0443081
cobaltcyan <cobaltcyan.park@gmail.com>
2023-08-23 14:07:30 +0900

    [Fix] TripCrateDto field 제거

--- src/main/kotlin/kr/kro/tripsketch/dto/TripCreateDto.kt
+++ src/main/kotlin/kr/kro/tripsketch/dto/TripCreateDto.kt
@@ -3,8 +3,6 @@ package kr.kro.tripsketch.dto
 import java.time.LocalDateTime
 
 data class TripCreateDto(
-    var id: String? = null,
-    var userEmail: String,
     var title: String,
     var content: String,
     var location: String? = null,
