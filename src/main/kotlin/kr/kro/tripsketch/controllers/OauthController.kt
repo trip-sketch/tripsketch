998633bc9650cf6bf7b7f4248b396cfd42413a31
HOJUN <bmp.tom@yahoo.com>
2023-08-24 22:13:55 +0900

    [modify] Login Logic Changed

--- src/main/kotlin/kr/kro/tripsketch/controllers/OauthController.kt
+++ src/main/kotlin/kr/kro/tripsketch/controllers/OauthController.kt
@@ -31,24 +31,22 @@ class OauthController(
 
         val oneTimeCode = authService.generateOneTimeCodeForToken(tokenResponse)
         val encryptedOneTimeCode = EncryptionUtils.encryptAES(oneTimeCode) // oneTimeCode 암호화
-        val redirectUrl = "https://port-0-tripsketch-kvmh2mljz6ccl7.sel4.cloudtype.app/api/oauth/kakao/redirect?encryptedOneTimeCode=$encryptedOneTimeCode"
+        val redirectUrl = "https://port-0-tripsketch-kvmh2mljz6ccl7.sel4.cloudtype.app/api/oauth/kakao/redirect?code=$encryptedOneTimeCode"
         return ResponseEntity.status(302).header("Location", redirectUrl).build()
     }
 
     @GetMapping("/retrieveToken")
-    fun retrieveToken(@RequestParam encryptedOneTimeCode: String, @RequestParam pushToken: String): ResponseEntity<TokenResponse> {
-        val decryptedOneTimeCode = EncryptionUtils.decryptAES(encryptedOneTimeCode) // encryptedOneTimeCode 복호화
-        val tokenResponse = authService.retrieveTokenByOneTimeCode(decryptedOneTimeCode, pushToken)
+    fun retrieveToken(@RequestParam code: String, @RequestParam token: String): ResponseEntity<TokenResponse> {
+        val decryptedOneTimeCode = EncryptionUtils.decryptAES(code) // encryptedOneTimeCode 복호화
+        val tokenResponse = authService.retrieveTokenByOneTimeCode(decryptedOneTimeCode, token)
             ?: return ResponseEntity.status(404).build() // 코드가 없을 경우 404 반환
         return ResponseEntity.ok(tokenResponse)
     }
 
-
     @GetMapping("/redirect")
-    fun handleEncryptedOneTimeCode(@RequestParam encryptedOneTimeCode: String): ResponseEntity<Void> {
+    fun handleEncryptedOneTimeCode(@RequestParam code: String): ResponseEntity<Void> {
         return ResponseEntity.ok().build() // Body가 없는 200 OK 응답 반환
     }
-
 
     @PostMapping("/refreshToken")
     @ApiResponse(responseCode = "200", description = "카카오 토큰 갱신이 성공적으로 완료되었습니다.")
@@ -62,7 +60,6 @@ class OauthController(
                 .body("Unable to refresh the Kakao token. Please check the provided refresh token.")
         }
     }
-
 
 //    @GetMapping("/code")
 //    @ApiResponse(responseCode = "200", description = "카카오 코드를 성공적으로 반환합니다.")
