4f800872ed524009b3e1d35fe680232ed25eb542
HOJUN <bmp.tom@yahoo.com>
2023-08-24 21:53:58 +0900

    [modify] Login Logic Changed

--- src/main/kotlin/kr/kro/tripsketch/controllers/OauthController.kt
+++ src/main/kotlin/kr/kro/tripsketch/controllers/OauthController.kt
@@ -24,44 +24,29 @@ class OauthController(
         return ResponseEntity.status(302).build()
     }
 
-
     @GetMapping("/callback")
     fun kakaoCallback(@RequestParam code: String): ResponseEntity<Any> {
         val tokenResponse = authService.authenticateViaKakao(code)
             ?: return ResponseEntity.status(400).body("Authentication failed.")
 
         val oneTimeCode = authService.generateOneTimeCodeForToken(tokenResponse)
-
-        val redirectUrl = "https://port-0-tripsketch-kvmh2mljz6ccl7.sel4.cloudtype.app/api/oauth/kakao/callback?oneTimeCode=$oneTimeCode"
+        val encryptedOneTimeCode = EncryptionUtils.encryptAES(oneTimeCode) // oneTimeCode 암호화
+        val redirectUrl = "https://port-0-tripsketch-kvmh2mljz6ccl7.sel4.cloudtype.app/api/oauth/kakao/redirect?encryptedOneTimeCode=$encryptedOneTimeCode"
         return ResponseEntity.status(302).header("Location", redirectUrl).build()
     }
 
     @GetMapping("/retrieveToken")
-    fun retrieveToken(@RequestParam oneTimeCode: String, @RequestParam pushToken: String): ResponseEntity<TokenResponse> {
-        val tokenResponse = authService.retrieveTokenByOneTimeCode(oneTimeCode, pushToken)
+    fun retrieveToken(@RequestParam encryptedOneTimeCode: String, @RequestParam pushToken: String): ResponseEntity<TokenResponse> {
+        val decryptedOneTimeCode = EncryptionUtils.decryptAES(encryptedOneTimeCode) // encryptedOneTimeCode 복호화
+        val tokenResponse = authService.retrieveTokenByOneTimeCode(decryptedOneTimeCode, pushToken)
             ?: return ResponseEntity.status(404).build() // 코드가 없을 경우 404 반환
-
         return ResponseEntity.ok(tokenResponse)
     }
 
-    @GetMapping("/code")
-    @ApiResponse(responseCode = "200", description = "카카오 코드를 성공적으로 반환합니다.")
-    fun kakaoCode(@RequestParam code: String): ResponseEntity<Void> {
-        val encryptedCode = EncryptionUtils.encryptAES(code)
-        return ResponseEntity.ok()
-            .header("X-Encrypted-Code", encryptedCode)  // encryptedCode 'X-Encrypted-Code'라는 헤더로 설정
-            .build()
-    }
 
-
-    @PostMapping("/login")
-    @ApiResponse(responseCode = "200", description = "카카오 로그인이 성공적으로 완료되었습니다.")
-    @ApiResponse(responseCode = "400", description = "인증에 실패했습니다.")
-    fun kakaoLogin(@RequestBody request: KakaoLoginRequest): ResponseEntity<Any> {
-        val decryptedCode = EncryptionUtils.decryptAES(request.code) // 복호화 로직 추가
-        val tokenResponse = authService.authenticateViaKakao(decryptedCode)
-            ?: return ResponseEntity.status(400).body("Authentication failed.")
-        return ResponseEntity.ok().body(tokenResponse)
+    @GetMapping("/api/oauth/kakao/redirect")
+    fun handleEncryptedOneTimeCode(@RequestParam encryptedOneTimeCode: String): ResponseEntity<Void> {
+        return ResponseEntity.ok().build() // Body가 없는 200 OK 응답 반환
     }
 
 
@@ -77,4 +62,26 @@ class OauthController(
                 .body("Unable to refresh the Kakao token. Please check the provided refresh token.")
         }
     }
+
+
+//    @GetMapping("/code")
+//    @ApiResponse(responseCode = "200", description = "카카오 코드를 성공적으로 반환합니다.")
+//    fun kakaoCode(@RequestParam code: String): ResponseEntity<Void> {
+//        val encryptedCode = EncryptionUtils.encryptAES(code)
+//        return ResponseEntity.ok()
+//            .header("X-Encrypted-Code", encryptedCode)  // encryptedCode 'X-Encrypted-Code'라는 헤더로 설정
+//            .build()
+//    }
+//
+//
+//    @PostMapping("/login")
+//    @ApiResponse(responseCode = "200", description = "카카오 로그인이 성공적으로 완료되었습니다.")
+//    @ApiResponse(responseCode = "400", description = "인증에 실패했습니다.")
+//    fun kakaoLogin(@RequestBody request: KakaoLoginRequest): ResponseEntity<Any> {
+//        val decryptedCode = EncryptionUtils.decryptAES(request.code) // 복호화 로직 추가
+//        val tokenResponse = authService.authenticateViaKakao(decryptedCode)
+//            ?: return ResponseEntity.status(400).body("Authentication failed.")
+//        return ResponseEntity.ok().body(tokenResponse)
+//    }
+
 }
