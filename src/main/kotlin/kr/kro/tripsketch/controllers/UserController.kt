aab04821b6bf748fb00d158ebdc52b2c90e6956e
cobaltcyan <cobaltcyan.park@gmail.com>
2023-08-24 15:38:28 +0900

    [Feat] user API 관련 코드 병합

--- src/main/kotlin/kr/kro/tripsketch/controllers/UserController.kt
+++ src/main/kotlin/kr/kro/tripsketch/controllers/UserController.kt
@@ -9,7 +9,6 @@ import kr.kro.tripsketch.exceptions.UnauthorizedException
 import kr.kro.tripsketch.services.UserService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
