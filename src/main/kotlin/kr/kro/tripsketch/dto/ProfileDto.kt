f0e6c8e3614b906fd2782c8277745fa2811d40b4
HOJUN <bmp.tom@yahoo.com>
2023-08-25 21:12:57 +0900

    [modify] validate

--- src/main/kotlin/kr/kro/tripsketch/dto/ProfileDto.kt
+++ src/main/kotlin/kr/kro/tripsketch/dto/ProfileDto.kt
@@ -2,6 +2,7 @@ package kr.kro.tripsketch.dto
 
 import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
+import org.hibernate.validator.constraints.Length
 
 data class ProfileDto(
     @field:Size(min = 3, max = 50, message = "별명은 3자에서 50자 사이여야 합니다.")
@@ -10,7 +11,6 @@ data class ProfileDto(
     @field:Size(max = 500, message = "소개는 최대 500자까지 가능합니다.")
     val introduction: String?,
 
-    @field:Pattern(regexp = "^(https?:\\/\\/)?([\\w\\-])+\\.{1}([a-zA-Z]{2,63})([\\/\\w-]*)*\\/?\\??([^\\&\\#\\n])*\\&?([^\\&\\#\\n])*$",
-        message = "올바른 URL 형식이어야 합니다.")
+    @field:Length(min = 5, max = 500, message = "텍스트 길이는 5자 이상 500자 이하이어야 합니다.")
     val profileImageUrl: String?
 )
