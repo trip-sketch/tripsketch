3ef25cd878c6596e1ddc7873221a738acdecd913
HOJUN <bmp.tom@yahoo.com>
2023-08-28 23:22:56 +0900

    [modify] push notification

--- src/main/kotlin/kr/kro/tripsketch/services/NotificationService.kt
+++ src/main/kotlin/kr/kro/tripsketch/services/NotificationService.kt
@@ -4,6 +4,7 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONArray
 import org.json.JSONObject
 import org.springframework.stereotype.Service
 import org.slf4j.LoggerFactory
@@ -16,9 +17,13 @@ class NotificationService(
     private val client = OkHttpClient()
     private val logger = LoggerFactory.getLogger(NotificationService::class.java)
 
-    fun sendPushNotification(email: String, title: String, body: String) {
-        val userToken = getUserToken(email) ?: throw IllegalArgumentException("User token not found for $email")
-        sendExpoPushNotification(userToken, title, body)
+    fun sendPushNotification(emails: List<String>, title: String, body: String) {
+        val tokens = emails.mapNotNull { getUserToken(it) }
+        if (tokens.isNotEmpty()) {
+            sendExpoPushNotification(tokens, title, body)
+        } else {
+            logger.warn("No valid push tokens found for the given emails.")
+        }
     }
 
     private fun getUserToken(email: String): String? {
@@ -26,13 +31,28 @@ class NotificationService(
         return user?.expoPushToken
     }
 
-    private fun sendExpoPushNotification(pushToken: String, title: String, message: String) {
-        val json = JSONObject()
-            .put("to", pushToken)
-            .put("title", title)
-            .put("body", message)
+    private fun sendExpoPushNotification(
+        pushTokens: List<String>,
+        title: String,
+        message: String,
+        sound: String? = null,
+        badge: Int? = null
+    ) {
+        val jsonArray = JSONArray()
 
-        val requestBody = json.toString()
+        pushTokens.forEach { token ->
+            val json = JSONObject()
+                .put("to", "ExponentPushToken[$token]")
+                .put("title", title)
+                .put("body", message)
+
+            sound?.let { json.put("sound", it) }
+            badge?.let { json.put("badge", it) }
+
+            jsonArray.put(json)
+        }
+
+        val requestBody = jsonArray.toString()
             .toRequestBody("application/json; charset=utf-8".toMediaType())
 
         val request = Request.Builder()
