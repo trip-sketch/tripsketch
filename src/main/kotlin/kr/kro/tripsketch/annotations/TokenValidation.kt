3581e2bb1395c3594ba3d4ddae145d4847eef62f
HOJUN <bmp.tom@yahoo.com>
2023-08-29 21:48:19 +0900

    [remove] Validation

--- src/main/kotlin/kr/kro/tripsketch/annotations/TokenValidation.kt
+++
@@ -1,7 +0,0 @@
-package kr.kro.tripsketch.annotations
-
-@Target(AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class TokenValidation(
-    val adminOnly: Boolean = false // 관리자만 접근 가능한 API인지 표시하는 속성
-)
