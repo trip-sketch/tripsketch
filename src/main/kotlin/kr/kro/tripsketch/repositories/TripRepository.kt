f00a95b78b67e782e2b59934b104fb301b9fb393
cobaltcyan <cobaltcyan.park@gmail.com>
2023-09-03 16:15:36 +0900

    [Feat] trip API - getAllTripsByUser 수정

--- src/main/kotlin/kr/kro/tripsketch/repositories/TripRepository.kt
+++ src/main/kotlin/kr/kro/tripsketch/repositories/TripRepository.kt
@@ -3,6 +3,8 @@ package kr.kro.tripsketch.repositories
 import kr.kro.tripsketch.domain.Trip
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
+//import org.springframework.data.jpa.repository.JpaRepository
+//import org.springframework.data.jpa.repository.Query
 
 @Repository
 interface TripRepository : MongoRepository<Trip, String> {
@@ -14,6 +16,9 @@ interface TripRepository : MongoRepository<Trip, String> {
     // isHidden 값이 false인 게시물 조회
     fun findByIsHiddenIsFalse(): Set<Trip>
 
+    // isHidden이 false이고 email이 일치하는 여행을 가져오는 메서드
+    fun findByIsHiddenIsFalseAndEmail(email: String): Set<Trip>
+
     // isHidden 값이 false이고, id가 일치하는 게시물 조회
     fun findByIdAndIsHiddenIsFalse(id: String): Trip?
 
@@ -22,4 +27,8 @@ interface TripRepository : MongoRepository<Trip, String> {
 
     // isPublic 값이 true이고 isHidden 값이 false인 게시물 조회
     fun findByIsPublicIsTrueAndIsHiddenIsFalse(): Set<Trip>
+
+
+    // email 조건이 맞고, isPublic 값이 true이고 isHidden 값이 false인 게시물 조회
+    fun findByIsPublicIsTrueAndIsHiddenIsFalseAndEmail(email: String): Set<Trip>
 }
