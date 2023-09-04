cae33bfb960c38f8360092d0e0b9a2ed2b5f7e5e
cobaltcyan <cobaltcyan.park@gmail.com>
2023-09-04 22:48:37 +0900

    [Feat] trip API - 검색 기능 구현 중

--- src/main/kotlin/kr/kro/tripsketch/repositories/TripRepository.kt
+++ src/main/kotlin/kr/kro/tripsketch/repositories/TripRepository.kt
@@ -13,25 +13,25 @@ interface TripRepository : MongoRepository<Trip, String> {
     // tripLikes 배열의 길이를 조회하는 메소드
     fun countByTripLikes(id: String): Long
 
-    // isHidden 값이 false인 게시물 조회
+    // 게시글 조회
     fun findByIsHiddenIsFalse(): Set<Trip>
 
-    // isHidden이 false이고 email이 일치하는 여행을 가져오는 메서드
+    // 로그인 사용자가 작성한 게시글 조회
     fun findByIsHiddenIsFalseAndEmail(email: String): Set<Trip>
 
-    // isHidden 값이 false이고, id가 일치하는 게시물 조회
+    // trip id가 일치하는 게시글 조회
     fun findByIdAndIsHiddenIsFalse(id: String): Trip?
 
-    // email과 isHidden 값이 false인 게시물 조회
+    // 로그인 사용자 외 작성자가 작성한 게시글 조회
     fun findTripByEmailAndIsHiddenIsFalse(email: String): Set<Trip>
 
-    // isPublic 값이 true이고 isHidden 값이 false인 게시물 조회
+    // 전체공개 게시글 조회
     fun findByIsPublicIsTrueAndIsHiddenIsFalse(email: String = ""): Set<Trip>
 
 //    @Query("SELECT t FROM Trip t WHERE t.isPublic = true AND t.isHidden = false AND t.email IN :emails")
 //    fun findByIsPublicIsTrueAndIsHiddenIsFalse(@Param("email") emails: Set<String>): Set<Trip>
 
-    // email 조건이 맞고, isPublic 값이 true이고 isHidden 값이 false인 게시물 조회
+    // email 조건이 맞으면서 전체공개인 게시글 조회
     fun findByIsPublicIsTrueAndIsHiddenIsFalseAndEmail(email: String): Set<Trip>
     fun findByIsPublicIsTrueAndIsHiddenIsFalseAndEmail(emails: Set<String>): Set<Trip>
 
