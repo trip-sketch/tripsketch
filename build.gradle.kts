46d0c8ed92bdffdd4732e450ca6b69a38201df16
HOJUN <bmp.tom@yahoo.com>
2023-09-14 18:03:12 +0900

    [rollback] Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ group = "kr.kro"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_20
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
@@ -103,7 +103,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "20"
+        jvmTarget = "17"
     }
 }
 
