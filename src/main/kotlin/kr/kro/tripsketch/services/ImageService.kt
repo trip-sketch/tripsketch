8a2c472890cf97968a66b2137224d983c4927f84
HOJUN <bmp.tom@yahoo.com>
2023-09-14 21:49:39 +0900

    [add] Image Compress

--- src/main/kotlin/kr/kro/tripsketch/services/ImageService.kt
+++ src/main/kotlin/kr/kro/tripsketch/services/ImageService.kt
@@ -3,12 +3,47 @@ package kr.kro.tripsketch.services
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.net.URI
+import java.awt.image.BufferedImage
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.InputStream
+import javax.imageio.ImageIO
+
 
 @Service
 class ImageService(private val s3Service: S3Service) {
 
+    fun compressImage(file: MultipartFile): ByteArray {
+        val originalImage: BufferedImage = ImageIO.read(ByteArrayInputStream(file.bytes))
+
+        val width = originalImage.width / 2
+        val height = originalImage.height / 2
+
+        val compressedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+        compressedImage.createGraphics().drawImage(originalImage, 0, 0, width, height, null)
+
+        val os = ByteArrayOutputStream()
+
+        // Determine the format name based on file extension
+        val formatName = file.originalFilename?.substringAfterLast('.', "jpg") ?: "jpg"
+
+        ImageIO.write(compressedImage, formatName, os)
+
+        return os.toByteArray()
+    }
+
     fun uploadImage(dir: String, file: MultipartFile): String {
-        val (url, _) = s3Service.uploadFile(dir, file)
+        val compressedImageBytes = compressImage(file)
+
+        val compressedFile = ByteArrayMultipartFile(
+            file.name,
+            file.originalFilename ?: "",
+            file.contentType,
+            compressedImageBytes
+        )
+
+        val (url, _) = s3Service.uploadFile(dir, compressedFile)
         return url
     }
 
@@ -27,6 +62,29 @@ class ImageService(private val s3Service: S3Service) {
         return Pair(dir, key)
     }
 
+    class ByteArrayMultipartFile(
+        private val fileName: String,
+        private val originalFileName: String,
+        private val contentType: String?,
+        private val content: ByteArray
+    ) : MultipartFile {
+        override fun getName(): String = fileName
 
+        override fun getOriginalFilename(): String? = originalFileName
+
+        override fun getContentType(): String? = contentType
+
+        override fun isEmpty(): Boolean = content.isEmpty()
+
+        override fun getSize(): Long = content.size.toLong()
+
+        override fun getBytes(): ByteArray = content
+
+        override fun getInputStream(): InputStream = ByteArrayInputStream(content)
+
+        override fun transferTo(dest: File) {
+            dest.writeBytes(content)
+        }
+    }
 
 }
