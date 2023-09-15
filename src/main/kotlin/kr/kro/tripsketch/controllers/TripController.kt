c83cd67d04372cf034f1ba7ea05e3467b32e0ea7
cobaltcyan <cobaltcyan.park@gmail.com>
2023-09-16 07:01:55 +0900

    [Feat, Fix] getAllTripsByUser repository 수정, 페이지네이션 및 조회순 정렬 추가, 에러처리 수정

--- src/main/kotlin/kr/kro/tripsketch/controllers/TripController.kt
+++ src/main/kotlin/kr/kro/tripsketch/controllers/TripController.kt
@@ -95,10 +95,37 @@ class TripController(private val tripService: TripService) {
 //    }
 
     @GetMapping("/trips")
-    fun getAllTripsByUser(req: HttpServletRequest): ResponseEntity<Set<TripDto>> {
-        val memberId = req.getAttribute("memberId") as Long
-        val findTrips = tripService.getAllTripsByUser(memberId)
-        return ResponseEntity.ok(findTrips)
+    fun getAllTripsByUser(
+        req: HttpServletRequest,
+        @RequestParam("page", required = false, defaultValue = "1") page: Int,
+        @RequestParam("size", required = false, defaultValue = "10") size: Int,
+        @RequestParam("sortType", required = false, defaultValue = "1") sortType: Int
+    ): ResponseEntity<Any> {
+        return try {
+            val memberId = req.getAttribute("memberId") as Long
+            val sort = when (sortType) {
+                1 -> Sort.by(Sort.Direction.DESC, "createdAt")
+                -1 -> Sort.by(Sort.Direction.ASC, "createdAt")
+                2 -> Sort.by(
+                    Sort.Order(Sort.Direction.DESC, "views"),
+                    Sort.Order(Sort.Direction.DESC, "createdAt")
+                )
+                -2 -> Sort.by(
+                    Sort.Order(Sort.Direction.ASC, "views"),
+                    Sort.Order(Sort.Direction.DESC, "createdAt")
+                )
+                else -> throw IllegalArgumentException("Invalid sort type")
+            }
+            val pageable: Pageable = PageRequest.of(page - 1, size, sort)
+            val findTrips = tripService.getAllTripsByUser(memberId, pageable)
+            if (findTrips.isNotEmpty()) {
+                ResponseEntity.status(HttpStatus.OK).body(findTrips)
+            } else {
+                ResponseEntity.status(HttpStatus.OK).body(mapOf("message" to "조회되는 게시물이 없습니다."))
+            }
+        } catch (e: IllegalArgumentException) {
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(mapOf("message" to (e.message ?: "")))
+        }
     }
 
     @GetMapping("/trips/mytrips")
