e8d76c17fe848b7c63d21bb55423fca067a496ed
cobaltcyan <cobaltcyan.park@gmail.com>
2023-09-17 00:50:10 +0900

    [Fix] trip API - getTripById 관련 로직 수정

--- src/main/kotlin/kr/kro/tripsketch/repositories/TripRepository.kt
+++ src/main/kotlin/kr/kro/tripsketch/repositories/TripRepository.kt
@@ -27,7 +27,7 @@ interface TripRepository : MongoRepository<Trip, String> {
     // trip id가 일치하는 게시글 조회
     fun findByIdAndIsHiddenIsFalse(id: String): Trip?
 
-    // public true, hidden false 인 trip id가 일치하는 게시글 조회
+    // trip id가 일치하는 전체공개 게시글 조회
     fun findByIdAndIsPublicIsTrueAndIsHiddenIsFalse(id: String): Trip?
 
     // 로그인 사용자 외 작성자가 작성한 게시글 조회
