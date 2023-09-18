9dfb4a6e0b2b38ad1276b9ea1e649474395477b2
고병욱 <rhquddnr95@naver.com>
2023-09-18 17:10:49 +0900

    [modify] TripUpdateDto/TripCreateDto location 제거

--- src/main/kotlin/kr/kro/tripsketch/dto/TripUpdateDto.kt
+++ src/main/kotlin/kr/kro/tripsketch/dto/TripUpdateDto.kt
@@ -18,7 +18,6 @@ data class TripUpdateDto(
     @field:Length(min = 5, max = 100, message = "텍스트 길이가 5자 이상이어야 합니다.")
     var content: String,
 
-    var location: String? = null,
     var startedAt: LocalDate? = LocalDate.now(),
     var endAt: LocalDate? = LocalDate.now(),
     var isPublic: Boolean? = true,
