cd7e53985eddb0fd812b52137837004c633b3b7c
고병욱 <rhquddnr95@naver.com>
2023-09-20 20:12:45 +0900

    [feat] Comment 관련 dto 문서화

--- src/main/kotlin/kr/kro/tripsketch/dto/CommentChildrenCreateDto.kt
+++ src/main/kotlin/kr/kro/tripsketch/dto/CommentChildrenCreateDto.kt
@@ -3,6 +3,14 @@ package kr.kro.tripsketch.dto
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
 
+/**
+ * 대댓글 생성 시 요청시 사용할 DTO 클래스입니다.
+ *
+ * @property tripId 게시물 아이디
+ * @property content 댓글 내용
+ * @property replyToNickname 답글 대상 닉네임
+ * @author BYEONGUK KO
+ */
 data class CommentChildrenCreateDto(
     @field:NotBlank(message = "게시물 아이디(tripId)는 필수 항목입니다.")
     val tripId: String,
