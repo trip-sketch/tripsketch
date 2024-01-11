30538a742bb5d62eb1959e80ef96c9fbbf0ad7d6
Hojun Song <bmp.tom@yahoo.com>
2024-01-11 22:17:53 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,7 +79,7 @@ dependencies {
     implementation("io.github.cdimascio:java-dotenv:5.2.2")
 
     // Amazon SDK
-    implementation("software.amazon.awssdk:apache-client:2.22.13")
+    implementation("software.amazon.awssdk:s3:2.22.13")
 
     // 테스트 프레임워크
     implementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
